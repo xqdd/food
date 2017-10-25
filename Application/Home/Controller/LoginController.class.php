1867302a8c71664dbb7a7ff86ed983a24d2525d8
XQDD <1032453509@qq.com>
2017-10-25 17:27:42 +0800

    solved concurrency issues.

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -1,11 +1,17 @@
 <?php
+
 namespace Home\Controller;
+
 use Think\Controller;
-class LoginController extends Controller {
 
-    public function test(){
+class LoginController extends Controller
+{
 
-        $map = rand(9069,9293);
+    public function test()
+    {
+
+
+        $map = rand(5259, 5476);
         //找到班级id
 
         //$mapClass['cl_name'] = $_SESSION['sdSession']['sd_class'];
@@ -22,93 +28,121 @@ class LoginController extends Controller {
         //$flag = M('classcontent')->field('cc_id')->where($mapCC)->find();
 
 
+        //使用文件锁
+        $fp = fopen(__DIR__.DIRECTORY_SEPARATOR."select_lock", "r") or die("打开文件失败");
 
-        //判断这门课是否被选了
-        $flag1 = M('studentcontent')->field('sc_id')->where($mapSC)->find();
-        //dump($flag1);
-        //判断这门课是否被选了
+        if (flock($fp, LOCK_EX | LOCK_NB)) {
+            //判断这门课是否被选了
+            $flag1 = M('studentcontent')->field('sc_id')->where($mapSC)->find();
+            //dump($flag1);
+            //判断这门课是否被选了
 
-        if($flag1 == null){
-            $data['sc_student_id'] = $map;
-            $data['sc_content_id'] = $contentId;
-            if(M('studentcontent')->add($data)){
-                if(D('content')->where($mapContent)->setInc('ct_seletctd_num',1)){
-                    echo "<script>alert('选课成功')</script>";
-                }else{
-                    echo "<script>alert('选课失败')</script>";
+            if ($flag1 == null) {
+                $data['sc_student_id'] = $map;
+                $data['sc_content_id'] = $contentId;
+                if (M('studentcontent')->add($data)) {
+                    if (D('content')->where($mapContent)->setInc('ct_seletctd_num', 1)) {
+                        echo "<script>alert('选课成功')</script>";
+                    } else {
+                        echo "<script>alert('选课失败')</script>";
+                    }
                 }
+            } else {
+                echo "<script>alert('该课题已经被选了')</script>";
             }
+            //释放锁
+            flock($fp, LOCK_UN);
+        } else {
+            echo "<script>alert('系统繁忙，请重试')</script>";
+            return;
         }
+        //关闭文件
+        fclose($fp);
+
 
         $Model = new \Home\Model\PersonModel();
         $listArray = $Model->dealWithIndex($map);
 
+
+        $this->assign('listArray', $listArray);
+        $this->display('index');
+
+
     }
 
     //显示登录页面并验证
-	public function index(){
-		if (IS_POST) {
-			//获取验证码、用户名和密码
-			$username = I('sd_num');
-			$password = I('sd_pwd');
-			$captcha = I('captcha');
-			//验证,注意顺序
-			//先检查验证码
-			$verify = new \Think\Verify();
-			if (!$verify->check($captcha)){
-				$this->assign('marks',"验证码错误!");
-				$this->display('index');
-				return;
-			}
-			//再来检查用户名和密码,调用模型来完成
-			if ($this->checkUser($username,$password)) {
-				$this->redirect('Index/index');
-			} else {
-				$this->assign('marks',"密码错误!");
-				$this->display('index');
-				return;
-			}
-			return;
-		} 
-		$this->display();
-	}
-	//验证用户名和密码
-	public function checkUser($username,$password){
-		$Dao = D('Student');
-		$condition['sd_num'] = $username;
-		$condition['sd_pwd'] = $password;
-		if ($sdSession = $Dao->where($condition)->find()) {
-			//成功，保存session标识，并跳转到首页
-			session('sdSession',$sdSession);
-			return true;
-		} else {
-			return false;
-		}
-	}
-	//生成验证码
-	public function code(){
-		// 使用tp自带的验证码类
-		ob_clean();//用来丢弃输出缓冲区中的内容，如果你的网站有许多生成的图片类文件，那么想要访问正确，就要经常清除缓冲区
-		
-		//配置参数
-		$config = array(
-		    'fontSize' => 20 ,//验证码字体大小
-		    'length' => 4 ,//验证码位数
-		    'useNoise' => false ,//关闭验证码杂点
-		);
-		$Verify = new \Think\Verify($config);
-		$Verify->entry();
-	}
-	public function nameAjax(){
-    	$sd_num = $_GET['valueDel'];
-		$map['sd_num'] = $sd_num;
-		$Nowpic = M('Student' )->field('sd_num')->where($map)->find();
-		$this->ajaxReturn($Nowpic['sd_num']);
-	}
-	//注销
-	public function logout(){
-		session('[destroy]');
-		$this->redirect('Login/index');
-	}
-	
+    public function index()
+    {
+        if (IS_POST) {
+            //获取验证码、用户名和密码
+            $username = I('sd_num');
+            $password = I('sd_pwd');
+            $captcha = I('captcha');
+            //验证,注意顺序
+            //先检查验证码
+            $verify = new \Think\Verify();
+            if (!$verify->check($captcha)) {
+                $this->assign('marks', "验证码错误!");
+                $this->display('index');
+                return;
+            }
+            //再来检查用户名和密码,调用模型来完成
+            if ($this->checkUser($username, $password)) {
+                $this->redirect('Index/index');
+            } else {
+                $this->assign('marks', "密码错误!");
+                $this->display('index');
+                return;
+            }
+            return;
+        }
+        $this->display();
+    }
+
+    //验证用户名和密码
+    public function checkUser($username, $password)
+    {
+        $Dao = D('Student');
+        $condition['sd_num'] = $username;
+        $condition['sd_pwd'] = $password;
+        if ($sdSession = $Dao->where($condition)->find()) {
+            //成功，保存session标识，并跳转到首页
+            session('sdSession', $sdSession);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //生成验证码
+    public function code()
+    {
+        // 使用tp自带的验证码类
+        ob_clean();//用来丢弃输出缓冲区中的内容，如果你的网站有许多生成的图片类文件，那么想要访问正确，就要经常清除缓冲区
+
+        //配置参数
+        $config = array(
+            'fontSize' => 20,//验证码字体大小
+            'length' => 4,//验证码位数
+            'useNoise' => false,//关闭验证码杂点
+        );
+        $Verify = new \Think\Verify($config);
+        $Verify->entry();
+    }
+
+    public function nameAjax()
+    {
+        $sd_num = $_GET['valueDel'];
+        $map['sd_num'] = $sd_num;
+        $Nowpic = M('Student')->field('sd_num')->where($map)->find();
+        $this->ajaxReturn($Nowpic['sd_num']);
+    }
+
+    //注销
+    public function logout()
+    {
+        session('[destroy]');
+        $this->redirect('Login/index');
+    }
+
 }
--- Application/Home/Controller/PersonController.class.php
+++ Application/Home/Controller/PersonController.class.php
@@ -1,46 +1,55 @@
 <?php
+
 namespace Home\Controller;
+
 use Think\Controller;
-class PersonController extends Controller {
-    public function index(){
-		$map = $_SESSION['sdSession']['sd_id'];
-		//获取公告的id或名字
-		$announce = M('announce')->field('an_id,an_title')->select();
-		$this->assign('announce',$announce);
-		if(I('marks')){ 
-			$this->assign('marks',I('marks'));
-			$this->assign('pwd',I('pwd'));
-		}
-    	$Model = new \Home\Model\PersonModel();
-		$listArray = $Model->dealWithIndex($map);
+
+class PersonController extends Controller
+{
+    public function index()
+    {
+        $map = $_SESSION['sdSession']['sd_id'];
+        //获取公告的id或名字
+        $announce = M('announce')->field('an_id,an_title')->select();
+        $this->assign('announce', $announce);
+        if (I('marks')) {
+            $this->assign('marks', I('marks'));
+            $this->assign('pwd', I('pwd'));
+        }
+        $Model = new \Home\Model\PersonModel();
+        $listArray = $Model->dealWithIndex($map);
         //dump($listArray);
-		$this->assign('listArray',$listArray);
-    	$this->display();
+        $this->assign('listArray', $listArray);
+        $this->display();
     }
-	public function delete(){
-/*		echo I('get.content_id');
-*/		$map['sc_content_id'] = I('get.content_id');
-		$map['sc_student_id'] = $_SESSION['sdSession']['sd_id'];
-		$map1['ct_id'] = I('get.content_id');
-		$Dao = D('studentcontent')->where($map)->delete();
-        if($Dao){
-        	if(D('content')->where($map1)->setDec('ct_seletctd_num',1)){
-        		$this->redirect('Person/index');
-        	}
-        }else{
-        	$this->error("退选失败");
+
+    public function delete()
+    {
+        /*		echo I('get.content_id');
+        */
+        $map['sc_content_id'] = I('get.content_id');
+        $map['sc_student_id'] = $_SESSION['sdSession']['sd_id'];
+        $map1['ct_id'] = I('get.content_id');
+        $Dao = D('studentcontent')->where($map)->delete();
+        if ($Dao) {
+            if (D('content')->where($map1)->setDec('ct_seletctd_num', 1)) {
+                $this->redirect('Person/index');
+            }
+        } else {
+            $this->error("退选失败");
         }
-	}
-	public function selectContent(){
+    }
 
-		  $dates = D("date")->find('1');
-        $currTime=time();
+    public function selectContent()
+    {
 
-        if($dates["da_start_time"]>$currTime||$dates["da_end_time"]<$currTime){
-            $this->error("选课失败，现在不是选课时间",U('Index/index'));
-            return ;
+        $dates = D("date")->find('1');
+        $currTime = time();
+
+        if ($dates["da_start_time"] > $currTime || $dates["da_end_time"] < $currTime) {
+            $this->error("选课失败，现在不是选课时间", U('Index/index'));
+            return;
         }
-
 
 
         $map['sc_student_id'] = $_SESSION['sdSession']['sd_id'];
@@ -48,76 +57,98 @@ class PersonController extends Controller {
         $howlong = null;
         $howlong = $Model1->where($map)->select();
         //dump($howlong);
-        if(count($howlong)>0){
-            $this->error("选课失败，你已经选满课了",U('Person/index'));
-            return ;
-        }		
+        if (count($howlong) > 0) {
+            $this->error("选课失败，你已经选满课了", U('Person/index'));
+            return;
+        }
 
-		$map = $_SESSION['sdSession']['sd_id'];	
-		//找到班级id
-		//$mapClass['cl_name'] = $_SESSION['sdSession']['sd_class'];
-		//$class = M('class')->field('cl_id')->where($mapClass)->find();
-		$contentId = I('content_id');
+        $map = $_SESSION['sdSession']['sd_id'];
+        //找到班级id
+        //$mapClass['cl_name'] = $_SESSION['sdSession']['sd_class'];
+        //$class = M('class')->field('cl_id')->where($mapClass)->find();
+        $contentId = I('content_id');
 
-		//$flag查找是该班级是否有对应的课题
-		/*$mapCC['cc_class_id'] = $class['cl_id'];
-		$mapCC['cc_content_id'] = $contentId;*/
-		$mapContent['ct_id'] = $contentId;
-		$mapSC['sc_content_id'] = $contentId;
-		//$mapSC['sc_student_id'] = $map;
-		
-		//$flag = M('classcontent')->field('cc_id')->where($mapCC)->find();
-		
-		
-		
-		//判断这门课是否被选了
-		$flag1 = M('studentcontent')->field('sc_id')->where($mapSC)->find();
-		//dump($flag1);
-		//判断这门课是否被选了
-		
-			if($flag1 == null){
-				   $data['sc_student_id'] = $map;
-				   $data['sc_content_id'] = $contentId;
-				    if(M('studentcontent')->add($data)){
-					if(D('content')->where($mapContent)->setInc('ct_seletctd_num',1)){
-						
-						echo "<script>alert('选课成功')</script>";
-					}else{
-						echo "<script>alert('选课失败')</script>";
-					}
-				   }
-			}
-		
-    	$Model = new \Home\Model\PersonModel();
-		$listArray = $Model->dealWithIndex($map);
-		
-		$this->assign('listArray',$listArray);
-		$this->display('index');
-	}
-    public function resetPwd(){
-    	$mapId['sd_id'] = $_SESSION['sdSession']['sd_id'];	
-    	$model = M('student');
-    	$data['sd_pwd'] = I('newPwd');
-		$map['sd_pwd'] = $oldPwd = I('oldPwd');
-		if($model->field('sd_id')->where($map)->find()){
-			if($model->where($mapId)->save($data)){
-				$this->redirect('Person/index',array('marks'=>'1111','pwd'=>I('newPwd')));
-			}else{
-				$this->redirect('Person/index',array('marks'=>'00000'));
-			}
-		}else{
-			$this->redirect('Person/index',array('marks'=>'0000'));
-		}
+        //$flag查找是该班级是否有对应的课题
+        /*$mapCC['cc_class_id'] = $class['cl_id'];
+        $mapCC['cc_content_id'] = $contentId;*/
+        $mapContent['ct_id'] = $contentId;
+        $mapSC['sc_content_id'] = $contentId;
+        //$mapSC['sc_student_id'] = $map;
+
+        //$flag = M('classcontent')->field('cc_id')->where($mapCC)->find();
+
+
+
+
+        //使用文件锁解决并发问题
+        $fp = fopen(__DIR__.DIRECTORY_SEPARATOR."select_lock", "r") or die("打开文件失败");
+
+        if (flock($fp, LOCK_EX | LOCK_NB)) {
+            //判断这门课是否被选了
+            $flag1 = M('studentcontent')->field('sc_id')->where($mapSC)->find();
+            //dump($flag1);
+            //判断这门课是否被选了
+
+            if ($flag1 == null) {
+                $data['sc_student_id'] = $map;
+                $data['sc_content_id'] = $contentId;
+                if (M('studentcontent')->add($data)) {
+                    if (D('content')->where($mapContent)->setInc('ct_seletctd_num', 1)) {
+                        echo "<script>alert('选课成功')</script>";
+                    } else {
+                        echo "<script>alert('选课失败')</script>";
+                    }
+                }
+            } else {
+                echo "<script>alert('该课题已经被选了')</script>";
+            }
+            //释放锁
+            flock($fp, LOCK_UN);
+        } else {
+            echo "<script>alert('系统繁忙，请重试')</script>";
+            return;
+        }
+        //关闭文件
+        fclose($fp);
+
+
+
+
+        $Model = new \Home\Model\PersonModel();
+        $listArray = $Model->dealWithIndex($map);
+
+
+        $this->assign('listArray', $listArray);
+        $this->display('index');
     }
-	public function resetPhone(){
-		$mapId['sd_id'] = $_SESSION['sdSession']['sd_id'];	
-    	$model = M('student');
-    	$data['sd_phone'] = I('sd_phone');
-			if($model->where($mapId)->save($data)){
-				$_SESSION['sdSession']['sd_phone'] = $data['sd_phone'];
-				$this->redirect('Person/index');
-			}else{
-				$this->redirect('Person/index');
-			}
-	}
+
+    public function resetPwd()
+    {
+        $mapId['sd_id'] = $_SESSION['sdSession']['sd_id'];
+        $model = M('student');
+        $data['sd_pwd'] = I('newPwd');
+        $map['sd_pwd'] = $oldPwd = I('oldPwd');
+        if ($model->field('sd_id')->where($map)->find()) {
+            if ($model->where($mapId)->save($data)) {
+                $this->redirect('Person/index', array('marks' => '1111', 'pwd' => I('newPwd')));
+            } else {
+                $this->redirect('Person/index', array('marks' => '00000'));
+            }
+        } else {
+            $this->redirect('Person/index', array('marks' => '0000'));
+        }
+    }
+
+    public function resetPhone()
+    {
+        $mapId['sd_id'] = $_SESSION['sdSession']['sd_id'];
+        $model = M('student');
+        $data['sd_phone'] = I('sd_phone');
+        if ($model->where($mapId)->save($data)) {
+            $_SESSION['sdSession']['sd_phone'] = $data['sd_phone'];
+            $this->redirect('Person/index');
+        } else {
+            $this->redirect('Person/index');
+        }
+    }
 }
